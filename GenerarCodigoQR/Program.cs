1bba929a5d2d169e65c30a12707cd4992200be22
Renato Jose Dominguez German <49384961+rex7g@users.noreply.github.com>
2022-06-02 08:23:23 -0400

    Add project files.

---
+++ GenerarCodigoQR/Program.cs
@@ -0,0 +1,11 @@
+﻿using QRCoder;
+using System.Drawing;
+
+QRCodeGenerator qrGenerator = new QRCodeGenerator();
+QRCodeData qrCodeData = qrGenerator.CreateQrCode("Renato Dominguez", QRCodeGenerator.ECCLevel.Q);
+PngByteQRCode qrCode = new PngByteQRCode(qrCodeData);
+byte[] qrCodeImage = qrCode.GetGraphic(20);
+using var stream = new MemoryStream(qrCodeImage);
+Bitmap bitmap = new Bitmap(stream);
+bitmap.Save("qr.png");
+Console.WriteLine("Se guardo la imagen");
